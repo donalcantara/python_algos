6ceb0ccf09b7b1d8fdfc2115f5e5d52cb954909e
donalcantara <don.alcantara@yahoo.com>
2016-05-24 17:50:09 -0700

    first commit

---
+++ python.py
@@ -0,0 +1,16 @@
+# print all the numbers from 1-1000
+# for x in range(1000):
+# 	print x
+
+# print all the multiples of 5 equal to or less than 1000
+# x = 0
+# while x <= 1000:
+# 	print x
+# 	x += 5
+
+#print a sum of all the values in the list
+a = [1, 2, 5, 10, 255, 3]
+answer = 0
+for x in a:
+	answer += x
+print answer

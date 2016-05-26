8f7ccf82e05743905bbb4afd8be037d9334c91d7
donalcantara <don.alcantara@yahoo.com>
2016-05-25 17:34:19 -0700

    multiply list items by a number

--- python.py
+++ python.py
@@ -23,8 +23,47 @@
 # print answer/len(a)
 
 # print out numbers 1-2000 and if they're odd or even
-for x in range(2000):
-	if x % 2 == 0:
-		print "The number " + str(x) + " is an even number!"
-	else:
-		print "The number " + str(x) + " is an odd number!"
+# for x in range(2000):
+# 	if x % 2 == 0:
+# 		print "The number " + str(x) + " is an even number!"
+# 	else:
+# 		print "The number " + str(x) + " is an odd number!"
+
+#
+# for i in range(2):
+# 	grade = raw_input("Give me a test score")
+
+# 	print grade
+
+# 	if grade >= '90':
+# 		print "You got an A!"
+# 	elif grade >= '80':
+# 		print "You got a B!"
+# 	elif grade >= '70':
+# 		print "You got a C!"
+# 	elif grade >= '60':
+# 		print "You got a D!"
+# 	else:
+# 		print "You suck"
+
+
+# multiply all items in list by a number
+a = [4,6,5,2,3]
+
+def multiply(x,c):
+	for i in range(len(x)):
+		x[i] = x[i] * c
+	return x
+
+b = multiply(a,5)
+print b
+
+
+
+
+
+
+
+
+
+
